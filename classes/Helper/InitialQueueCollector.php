8f13d27ae45e10e77a510fe20be544c71a3637dc
mjansen <mjansen@databay.de>
2023-05-19 12:00:48 +0200

    Add ILIAS 8 compatibility

--- classes/Helper/InitialQueueCollector.php
+++ classes/Helper/InitialQueueCollector.php
@@ -1,24 +1,31 @@
 <?php
 
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
 namespace QU\LERQ\Helper;
 
-/**
- * Class InitialQueueCollector
- * @package QU\LERQ\Helper
- * @author Ralph Dittrich <dittrich@qualitus.de>
- */
 class InitialQueueCollector
 {
-    /** @var InitialQueueCollector */
-    protected static $instance;
+    private static ?InitialQueueCollector $instance = null;
 
-    /** @var array  */
-    private $tree = [];
+    /** @var array<int, array{0: int, 1: null|string}> */
+    private array $tree = [];
 
-    /**
-     * @return InitialQueueCollector
-     */
-    public static function singleton()
+    public static function singleton(): self
     {
         if (!self::$instance) {
             self::$instance = new self();
@@ -29,52 +36,51 @@ class InitialQueueCollector
 
     /**
      * Collect Base Data
-     *
      * Collects the base data, required to capture all other relevant queue data.
-     *
-     * @param int $start            LIMIT start
-     * @param int $end              LIMIT step
-     * @param string $only_type     Specific object type to get. Default "*" is used top get all.
-     * @return array
+     * @return list<array<string, mixed>>
      */
-    public function collectBaseDataFromDB($start = 0, $end = 1000, $only_type = '*'): array
+    public function collectBaseDataFromDB(int $start = 0, int $end = 1000, string $only_type = '*'): array
     {
         global $DIC;
 
         $result = $DIC->database()->query($this->getBaseDataQuery([
-            'rua.usr_id', 'oref.ref_id', 'oref.obj_id', 'rua.rol_id', 'od.type', 'ulm.status', 'ulm.status_changed'
-            ,'od.title' ,'cs.crs_start' ,'cs.crs_end'
+            'rua.usr_id',
+            'oref.ref_id',
+            'oref.obj_id',
+            'rua.rol_id',
+            'od.type',
+            'ulm.status',
+            'ulm.status_changed',
+            'od.title',
+            'cs.crs_start',
+            'cs.crs_end'
         ], 0, false, [$start, $end], ['oref.ref_id', 'ASC'], $only_type));
+
         return $DIC->database()->fetchAll($result);
     }
 
     /**
      * Count Base Data
-     *
      * Get the count of all rows, that could be collected by collectBaseDataFromDB
      * @see collectBaseDataFromDB
-     *
-     * @param string $only_type     Specific object type to get. Default "*" is used top get all.
-     * @return array
      */
-    public function countBaseDataFromDB($only_type = '*'): int
+    public function countBaseDataFromDB(string $only_type = '*'): int
     {
         global $DIC;
 
         $result = $DIC->database()->query($this->getBaseDataQuery([
-            'COUNT(rua.usr_id) as count'
+            'COUNT(rua.usr_id) AS count'
         ], 0, false, [], [], $only_type));
-        return (int) $DIC->database()->fetchAll($result)[0]['count'];
+
+        return (int) ($DIC->database()->fetchAll($result)[0]['count'] ?? 0);
     }
 
     /**
      * Collect User Data
-     *
      * collects all user data, enclosed by the list of user ids from collectBaseDataFromDB
-     * @see collectBaseDataFromDB
+     * @return array<int, array<string, mixed>>
      * @see getBaseDataQuery
-     *
-     * @return array
+     * @see collectBaseDataFromDB
      */
     public function collectUserDataFromDB(): array
     {
@@ -92,7 +98,6 @@ JOIN `object_data` `uod` ON `uod`.`obj_id` = `ud`.`usr_id`
 WHERE `ud`.`usr_id` IN (' . $user_id_subq . ')';
         $result = $DIC->database()->query($query);
 
-
         $udf_query = 'SELECT udf.usr_id, udf.field_id, udf.`value`
 FROM (
     SELECT usr_id, field_id, `value` FROM udf_text WHERE usr_id IN (' . $user_id_subq . ')
@@ -106,22 +111,25 @@ ORDER BY udf.usr_id';
         $udf_res = $DIC->database()->query($udf_query);
 
         $udf_data = [];
-        while($udf_row = $DIC->database()->fetchAssoc($udf_res)) {
-            if (!array_key_exists($udf_row['usr_id'], $udf_data)) {
-                $udf_data[$udf_row['usr_id']] = [];
+        while ($udf_row = $DIC->database()->fetchAssoc($udf_res)) {
+            if (!array_key_exists((int) $udf_row['usr_id'], $udf_data)) {
+                $udf_data[(int) $udf_row['usr_id']] = [];
             }
-            $udf_data[$udf_row['usr_id']]["f_" . $udf_row['field_id']] = (
-                isset($udf_row['value']) ? $udf_row['value'] : ""
+
+            $udf_data[(int) $udf_row['usr_id']]['f_' . $udf_row['field_id']] = (
+                $udf_row['value'] ?? ''
             );
         }
 
         $data = [];
-        while($row = $DIC->database()->fetchAssoc($result)) {
-
-            if (array_key_exists($row['usr_id'], $udf_data)) {
-                $row['udfdata'] = $udf_data[$row['usr_id']];
+        while ($row = $DIC->database()->fetchAssoc($result)) {
+            if (isset($udf_data[(int) $row['usr_id']])) {
+                $row['udfdata'] = $udf_data[(int) $row['usr_id']];
+            } else {
+                $row['udfdata'] = [];
             }
-            $data[$row['usr_id']] = $row;
+
+            $data[(int) $row['usr_id']] = $row;
         }
 
         return $data;
@@ -129,13 +137,11 @@ ORDER BY udf.usr_id';
 
     /**
      * Find parent course
-     *
      * Collects the tree, if not already done and walks through it, to find the parent course.
-     *
-     * @param int $ref_id   Ref ID of the current (non-course) object.
+     * @param int $ref_id Ref ID of the current (non-course) object.
      * @return int          Returns the parent Ref ID if a parent course is found. Otherwise -1.
      */
-    public function findParentCourse(int $ref_id)
+    public function findParentCourse(int $ref_id): int
     {
         global $DIC;
 
@@ -159,17 +165,23 @@ AND obd.type NOT IN (
 ORDER BY tr.child';
 
             $result = $DIC->database()->query($query);
-            while($row = $DIC->database()->fetchAssoc($result)) {
-                $this->tree[$row['child']] = [$row['parent'], $row['type']];
+            while ($row = $DIC->database()->fetchAssoc($result)) {
+                $this->tree[(int) $row['child']] = [(int) $row['parent'], $row['type']];
             }
         }
 
-        list($parent, $type) = $this->tree[$ref_id];
-        if (!isset($parent) || !isset($type)) {
+        if (!isset($this->tree[$ref_id])) {
             return -1;
         }
-        if ($type != 'crs') {
-            $crs_ref = $this->findParentCourse((int)$parent);
+
+        [$parent, $type] = $this->tree[$ref_id];
+
+        if (!isset($parent, $type)) {
+            return -1;
+        }
+
+        if ($type !== 'crs') {
+            $crs_ref = $this->findParentCourse($parent);
         } else {
             $crs_ref = $parent;
         }
@@ -179,13 +191,11 @@ ORDER BY tr.child';
 
     /**
      * Collect course data
-     *
      * Collect the base data of courses
-     *
-     * @param int $ref_id   Ref ID of the current (course) object.
-     * @return array
+     * @param int $ref_id Ref ID of the current (course) object.
+     * @return null|array<string, mixed>
      */
-    public function collectCourseDataByRefId(int $ref_id): array
+    public function collectCourseDataByRefId(int $ref_id): ?array
     {
         global $DIC;
 
@@ -201,7 +211,7 @@ ORDER BY tr.child';
         $result = $DIC->database()->query($query);
         $data = $DIC->database()->fetchAll($result);
 
-        return $data[0];
+        return $data[0] ?? null;
     }
 
     /**
@@ -209,16 +219,22 @@ ORDER BY tr.child';
      *
      * The query is required to get the correct assignment data for: user <> learning object
      *
-     * @param array $field_list     List of fields to get.
-     * @param int $ref_id           Lowest Ref ID to start at.
-     * @param bool $distinct        Distinct the first field of field list.
-     * @param array $page           Database limit pagination. Array [<int>Start , <int>End]
-     * @param array $order          Query result order. Array [<string>Field , <string>Direction]
-     * @param string $only_type     Specific object type to get. Default "*" is used top get all.
+     * @param array $field_list List of fields to get.
+     * @param int $ref_id Lowest Ref ID to start at.
+     * @param bool $distinct Distinct the first field of field list.
+     * @param array{0: int, 1: int}|array{} $page Database limit pagination. Array [<int>Start , <int>End]
+     * @param array{0: string, 1: string}|array{} $order Query result order. Array [<string>Field , <string>Direction]
+     * @param string $only_type Specific object type to get. Default "*" is used top get all.
      * @return string
      */
-    public function getBaseDataQuery(array $field_list, int $ref_id = 0, $distinct = false, $page = [], $order = [], $only_type = '*'): string
-    {
+    private function getBaseDataQuery(
+        array $field_list,
+        int $ref_id = 0,
+        bool $distinct = false,
+        array $page = [],
+        array $order = [],
+        string $only_type = '*'
+    ): string {
         $query = 'SELECT ' . ($distinct ? 'DISTINCT ' : '') . implode(',', $field_list) . '
 FROM `object_reference` `oref` 
 LEFT JOIN `rbac_fa` `rfa` 
@@ -234,8 +250,8 @@ LEFT JOIN `crs_settings` `cs`
     ON `od`.`obj_id` = `cs`.`obj_id`
 WHERE `rfa`.`assign` = "y" 
     AND `rua`.`rol_id` IS NOT NULL 
-    ' ;
-        if ($only_type != '*') {
+    ';
+        if ($only_type !== '*') {
             $query .= 'AND `od`.`type` = "' . $only_type . '" ';
         } else {
             $query .= 'AND `od`.`type` NOT IN ("rolf", "role") ';
@@ -245,16 +261,15 @@ WHERE `rfa`.`assign` = "y"
     AND `rua`.`usr_id` != 6
     AND `rua`.`usr_id` != 0
     AND `oref`.`deleted` IS NULL';
-        if (!empty($order)) {
+        if (count($order) === 2) {
             $query .= '
             ORDER BY ' . $order[0] . ' ' . $order[1] . ' ';
         }
-        if (!empty($page)) {
+        if (count($page) === 2) {
             $query .= '
             LIMIT ' . $page[0] . ', ' . $page[1] . ' ';
         }
 
         return $query;
     }
-
 }
